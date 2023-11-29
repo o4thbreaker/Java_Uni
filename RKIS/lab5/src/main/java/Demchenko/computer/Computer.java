4f9f46d6c461f0c116f6d21b22196ab489b7d417
o4thbreaker <146327700+o4thbreaker@users.noreply.github.com>
2023-11-30 02:05:03 +0700

    fix lab5

--- RKIS/lab5/src/main/java/Demchenko/computer/Computer.java
+++ RKIS/lab5/src/main/java/Demchenko/computer/Computer.java
@@ -1,8 +1,13 @@
 package Demchenko.computer;
 
 import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 @Table(name = "computer")
